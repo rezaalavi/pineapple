ffa916f0ba196ebe4816d0d9181625a083695474
Leonardo Correa <leonardoanalista@gmail.com>
2011-03-17 15:44:41 +1100

    added sandbox rails app for testing

---
+++ sandbox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox::Application.config.secret_token = 'fad6f8504d3810364e01554b402a285cfcbf1b3e530f99afaddbff2094d5a24ca16178da1f27b473c093f69c22c5c6d7b2e58b391a5d4f4d1615995e619f9581'
