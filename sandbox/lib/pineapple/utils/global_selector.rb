18d36b04f4290e02a033a65c6a27e9c76ca2060d
Reza Alavi <rezaalavi@rubyforge.org>
2011-05-11 21:08:39 +1000

    removing unnecessay files + noconflict for jquery

--- sandbox/lib/pineapple/utils/global_selector.rb
+++ sandbox/lib/pineapple/utils/global_selector.rb
@@ -3,14 +3,14 @@ module Actions
   def find_item selector
     
         @script += <<SCRIPT
-    var pineappleVars.item = $("##{selector}");    
+    var pineappleVars.item = jQuery("##{selector}");
     if(typeof pineappleVars.item.get(0) == "undefined"){
-      pineappleVars.item = $("input[name='#{selector}']");
+      pineappleVars.item = jQuery("input[name='#{selector}']");
       if(typeof pineappleVars.item.get(0) == "undefined"){
-        pineappleVars.item = $(":contains('#{selector}')");
+        pineappleVars.item = jQuery(":contains('#{selector}')");
       } 
       if(typeof pineappleVars.item.get(0) == "undefined"){
-        pineappleVars.item = $("#{selector}");
+        pineappleVars.item = jQuery("#{selector}");
       }       
     }    
 SCRIPT
